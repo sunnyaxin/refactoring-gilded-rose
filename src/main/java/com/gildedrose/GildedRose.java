acfa65909559f6657d9ec974042207b29161fa63
sunnyaxin <137126846@qq.com>
2020-03-01 22:07:51 +0800

    Move quality closely related logic from GlideRose to Product - Feature Envy.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,10 +2,6 @@ package com.gildedrose;
 
 import java.util.List;
 
-import static com.gildedrose.ProductRulesName.AGED_BRIE;
-import static com.gildedrose.ProductRulesName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT;
-import static com.gildedrose.ProductRulesName.SULFURAS_HAND_OF_RAGNAROS;
-
 class GildedRose {
     List<Product> products;
 
@@ -14,44 +10,7 @@ class GildedRose {
     }
 
     public void updateProducts() {
-        updateSellIn();
-        updateQuality();
-    }
-
-    private void updateSellIn() {
-        products.stream()
-                .filter(product -> !product.name.equals(SULFURAS_HAND_OF_RAGNAROS))
-                .forEach(product -> product.sellIn -= 1);
-    }
-
-    private void updateQuality() {
-        for (Product product : products) {
-            switch (product.name) {
-                case SULFURAS_HAND_OF_RAGNAROS:
-                    break;
-                case AGED_BRIE:
-                    if (product.quality < 50)
-                        product.quality += 1;
-                    if (product.quality < 50 && product.sellIn < 0)
-                        product.quality += 1;
-                    break;
-                case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT:
-                    if (product.quality < 50)
-                        product.quality += 1;
-                    if (product.quality < 50 && product.sellIn < 11)
-                        product.quality += 1;
-                    if (product.quality < 50 && product.sellIn < 6)
-                        product.quality += 1;
-                    if (product.sellIn < 0)
-                        product.quality = 0;
-                    break;
-                default:
-                    if (product.quality > 0)
-                        product.quality -= 1;
-                    if (product.quality > 0 && product.sellIn < 0)
-                        product.quality -= 1;
-                    break;
-            }
-        }
+        products.forEach(Product::updateSellIn);
+        products.forEach(Product::updateQuality);
     }
 }
--- src/main/java/com/gildedrose/Product.java
+++ src/main/java/com/gildedrose/Product.java
@@ -1,5 +1,9 @@
 package com.gildedrose;
 
+import static com.gildedrose.ProductRulesName.AGED_BRIE;
+import static com.gildedrose.ProductRulesName.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT;
+import static com.gildedrose.ProductRulesName.SULFURAS_HAND_OF_RAGNAROS;
+
 public class Product {
 
     public String name;
@@ -10,6 +14,41 @@ public class Product {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+    }
+
+    void updateSellIn() {
+        if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            sellIn -= 1;
+        }
+    }
+
+    void updateQuality() {
+        switch (name) {
+            case SULFURAS_HAND_OF_RAGNAROS:
+                break;
+            case AGED_BRIE:
+                if (quality < 50)
+                    quality += 1;
+                if (quality < 50 && sellIn < 0)
+                    quality += 1;
+                break;
+            case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT:
+                if (quality < 50)
+                    quality += 1;
+                if (quality < 50 && sellIn < 11)
+                    quality += 1;
+                if (quality < 50 && sellIn < 6)
+                    quality += 1;
+                if (sellIn < 0)
+                    quality = 0;
+                break;
+            default:
+                if (quality > 0)
+                    quality -= 1;
+                if (quality > 0 && sellIn < 0)
+                    quality -= 1;
+                break;
+        }
     }
 
     @Override
